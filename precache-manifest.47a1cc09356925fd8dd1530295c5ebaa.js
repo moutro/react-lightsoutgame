9e8b15ef88f3e16aa2782cbe458e4de9559f9be7
Jason B <philotimo@gmail.com>
2019-09-03 23:31:42 -0400

    Updates

---
+++ precache-manifest.47a1cc09356925fd8dd1530295c5ebaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4d6ca73e9bcc8ddfd0a",
+    "url": "/react-lightsoutgame/static/js/runtime~main.a4d6ca73.js"
+  },
+  {
+    "revision": "2454fe947f1432e50057",
+    "url": "/react-lightsoutgame/static/js/main.2454fe94.chunk.js"
+  },
+  {
+    "revision": "8832ac643a5ef291d616",
+    "url": "/react-lightsoutgame/static/js/1.8832ac64.chunk.js"
+  },
+  {
+    "revision": "2454fe947f1432e50057",
+    "url": "/react-lightsoutgame/static/css/main.894ffe26.chunk.css"
+  },
+  {
+    "revision": "0b086859b2192627c643d6c072781055",
+    "url": "/react-lightsoutgame/index.html"
+  }
+];
